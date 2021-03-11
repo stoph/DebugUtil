821da39d5c4c8f36b91a8b378781ab6e46de44f6
Christoph Khouri <christoph.khouri@gmail.com>
2021-03-11 17:56:13 -0500

    Adding __FILENAME__
    '/' is Mac specific. Use '\\' for Windows (I think :)

--- DebugUtil.h
+++ DebugUtil.h
@@ -1,22 +1,22 @@
 #ifdef ENABLE_DEBUG
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#define DEBUG_PRINT(str) \
-  Serial.print(str);
-#define DEBUG_PRINTLN(str) \
-  Serial.println(str);
-#define DEBUG_PRINT_VERBOSE(str) \
-  Serial.println(str); \
-  Serial.print(" ^^ ["); \
-  Serial.print(millis()); \
-  Serial.print("ms] in '"); \
-  Serial.print(__PRETTY_FUNCTION__); \
-  Serial.print("' from '"); \
-  Serial.print(__FILENAME__); \
-  Serial.print("' at line "); \
-  Serial.print(__LINE__); \
-  Serial.println();
+  #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+  #define DEBUG_PRINT(str) \
+    Serial.print(str);
+  #define DEBUG_PRINTLN(str) \
+    Serial.println(str);
+  #define DEBUG_PRINT_VERBOSE(str) \
+    Serial.println(str); \
+    Serial.print(" ^^ ["); \
+    Serial.print(millis()); \
+    Serial.print("ms] in '"); \
+    Serial.print(__PRETTY_FUNCTION__); \
+    Serial.print("' from '"); \
+    Serial.print(__FILENAME__); \
+    Serial.print("' at line "); \
+    Serial.print(__LINE__); \
+    Serial.println();
 #else
-#define DEBUG_PRINT(str)
-#define DEBUG_PRINTLN(str)
-#define DEBUG_PRINT_VERBOSE(str)
+  #define DEBUG_PRINT(str)
+  #define DEBUG_PRINTLN(str)
+  #define DEBUG_PRINT_VERBOSE(str)
 #endif
